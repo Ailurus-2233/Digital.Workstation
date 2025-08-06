8fd092376b555f0668b8276ffeec064124fd9e8c
Ailurus <Ailurus2233@outlook.com>
2025-08-06 22:58:51 +0800

    完成编译后的文件结构调整

---
+++ Core/Framework/SimpleWindow.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace DigitalWorkstation.Framework;
+
+public partial class SimpleWindow : Window
+{
+    public SimpleWindow()
+    {
+        InitializeComponent();
+    }
+}
