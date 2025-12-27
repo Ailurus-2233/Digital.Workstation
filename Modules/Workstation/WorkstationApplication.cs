5cd49d841efb243cfe21132227d7a29c4d83bfac
Ailurus <Ailurus2233@outlook.com>
2025-12-27 22:40:54 +0800

    feat(framework): 添加 UI 包含的主题样式和相关依赖
    - 在 Framework.csproj 中添加对 UIPackage 的项目引用
    - 更新 FrameworkApplication 初始化方法，添加主题样式支持
    - 在 MainWindow.axaml 中更新欢迎文本
    - 在 Launcher.cs 中启用开发者工具
    
    此提交增强了框架的主题样式支持，并改善了主窗口的欢迎信息。

--- Modules/Workstation/WorkstationApplication.cs
+++ Modules/Workstation/WorkstationApplication.cs
@@ -1,20 +1,10 @@
-﻿using Avalonia.Styling;
-using DigitalWorkstation.Core.Framework;
-using DigitalWorkstation.Core.UIPackage;
+﻿using DigitalWorkstation.Core.Framework;
 using DigitalWorkstation.DashBoard;
 
 namespace DigitalWorkstation.Workstation;
 
 public class WorkstationApplication : FrameworkApplication<MainWindow>
 {
-    public override void Initialize()
-    {
-        // Initialization logic here
-        RequestedThemeVariant = ThemeVariant.Default;
-        Styles.AddRange(new WorkstationTheme());
-        base.Initialize();
-    }
-
     protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
     {
         moduleCatalog.AddModule<DashBoardModule>();
