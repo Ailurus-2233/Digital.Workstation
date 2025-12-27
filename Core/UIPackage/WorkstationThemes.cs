d94d78d30c4c3ab3ceec7d5fc4fe79ef086adea9
Ailurus <Ailurus2233@outlook.com>
2025-12-27 22:08:00 +0800

    feat(uipackage): 添加 UI 包含的主题样式和相关依赖

---
+++ Core/UIPackage/WorkstationThemes.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia.Styling;
+using Semi.Avalonia;
+using Semi.Avalonia.ColorPicker;
+using Semi.Avalonia.DataGrid;
+using Semi.Avalonia.Dock;
+
+namespace DigitalWorkstation.Core.UIPackage;
+
+public class WorkstationTheme : Styles
+{
+    public WorkstationTheme()
+    {
+        Add(new SemiTheme());
+        Add(new Ursa.Themes.Semi.SemiTheme());
+        Add(new ColorPickerSemiTheme());
+        Add(new DataGridSemiTheme());
+        Add(new DockSemiTheme());
+    }
+}
